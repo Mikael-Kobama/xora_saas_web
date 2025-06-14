df51c17f7fec96c3597d39eb7b83eccd19b947fc
Sr. Kobama <125419153+Mikael-Kobama@users.noreply.github.com>
2025-06-13 21:37:54 -0300

    update

---
+++ src/components/FaqItem.jsx
@@ -0,0 +1,16 @@
+import clsx from "clsx";
+import { useState } from "react";
+import { SlideDown } from "react-slidedown";
+import "react-slidedown/lib/slidedown.css";
+
+const FaqItem = () => {
+  return (
+    <div className="relative z-2 mb-16">
+      <div className="group relative flex cursor-pointer items-center justify-between gap-10 px-7">
+        <div className="flex-1"></div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqItem;
